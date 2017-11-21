10cd353da28b0827ca623f46ebd142dc7b156945
malov.alexsander <malov.alexsander@yandex.ru>
2017-11-21 14:36:14 +0600

    Добавьте файлы проекта.

---
+++ LaborDBF/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LaborDBF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.allBase = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exel = new System.Windows.Forms.Button();
+            this.monthTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 122);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1235, 653);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // allBase
+            // 
+            this.allBase.Location = new System.Drawing.Point(1119, 31);
+            this.allBase.Name = "allBase";
+            this.allBase.Size = new System.Drawing.Size(89, 23);
+            this.allBase.TabIndex = 1;
+            this.allBase.Text = "All Base";
+            this.allBase.UseVisualStyleBackColor = true;
+            this.allBase.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Find";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(895, 31);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 5;
+            this.button11.Text = "Врачи";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(993, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Лаборанты";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // exel
+            // 
+            this.exel.Location = new System.Drawing.Point(1119, 71);
+            this.exel.Name = "exel";
+            this.exel.Size = new System.Drawing.Size(89, 23);
+            this.exel.TabIndex = 7;
+            this.exel.Text = "Exel";
+            this.exel.UseVisualStyleBackColor = true;
+            this.exel.Click += new System.EventHandler(this.exel_Click);
+            // 
+            // monthTextBox
+            // 
+            this.monthTextBox.Location = new System.Drawing.Point(630, 31);
+            this.monthTextBox.Name = "monthTextBox";
+            this.monthTextBox.Size = new System.Drawing.Size(171, 22);
+            this.monthTextBox.TabIndex = 8;
+            this.monthTextBox.TextChanged += new System.EventHandler(this.monthTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1249, 784);
+            this.Controls.Add(this.monthTextBox);
+            this.Controls.Add(this.exel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.allBase);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button allBase;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button exel;
+        private System.Windows.Forms.TextBox monthTextBox;
+    }
+}
+
---
+++ LaborDBF/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace LaborDBF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            if ( Convert.ToInt32(DateTime.Now.ToString("dd")) < 5 )
+            {
+                monthTextBox.Text = DateTime.Now.AddDays(-10).ToString("MMMMMMMMMMM");
+            }
+            else {
+                 monthTextBox.Text = DateTime.Now.ToString("MMMMMMMMMMM");     
+            }
+        }
+                
+
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dirFile = textBox1.Text;
+                string cadena = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dirFile + ";Extended Properties=dBASE IV;User ID=Admin;Password=;";
+                
+                OleDbConnection con = new OleDbConnection(cadena);
+
+                con.Open();
+                String sqlComand = "Select * FROM " + textBox2.Text;
+
+                OleDbDataAdapter adapter = new OleDbDataAdapter(sqlComand, con);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+
+                con.Close();
+
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (OleDbException exp)
+            {
+                MessageBox.Show("Error: " + exp.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                int lenghtDir = ofd.FileName.Length - ofd.SafeFileName.Length;
+                textBox1.Text = ofd.FileName.Substring(0, lenghtDir); 
+                textBox2.Text = ofd.SafeFileName;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {                
+                string dirFile = textBox1.Text;                
+                string cadena = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dirFile + ";Extended Properties=dBASE IV;User ID=Admin;Password=;";
+                
+                OleDbConnection con = new OleDbConnection(cadena);
+                con.Open();                 
+                
+                String sqlComand = "SELECT SHTAT.PCODE, SHTAT.FAM + ' ' + LEFT(SHTAT.IM, 1) + '.' + LEFT(SHTAT.OT, 1) + '.' AS Doctor, COUNT(MU.COD_U) as Summa " +
+                    "FROM MU LEFT JOIN SHTAT on MU.cod_u = SHTAT.PCODE group by PCODE, FAM, IM, OT ";
+                
+                OleDbDataAdapter adapter = new OleDbDataAdapter(sqlComand, con);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                con.Close();
+
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (OleDbException exp)
+            {
+                MessageBox.Show("Error: " + exp.Message);
+            }
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            string dirFile = textBox1.Text;
+            string cadena = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dirFile + ";Extended Properties=dBASE IV;User ID=Admin;Password=;";
+
+            OleDbConnection con = new OleDbConnection(cadena);
+            con.Open();
+
+            String sqlComand = "SELECT SHTAT.PCODE, SHTAT.FAM + ' ' + LEFT(SHTAT.IM, 1) + '.' + LEFT(SHTAT.OT, 1) + '.' AS Doctor, COUNT(MU.COD_U2) as Summa " +
+                "FROM MU LEFT JOIN SHTAT on MU.cod_u2 = SHTAT.PCODE group by PCODE, FAM, IM, OT ";
+            
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sqlComand, con);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+            con.Close();
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void exel_Click(object sender, EventArgs e)
+        {            
+            Excel.Application exApp = new Excel.Application();
+            exApp.Visible = true;
+            exApp.Workbooks.Add();
+            exApp.Columns.ColumnWidth = 22;
+
+            Worksheet workSheet = (Worksheet)exApp.ActiveSheet;
+            workSheet.Cells[1, 2] = monthTextBox.Text;
+            (workSheet.Cells[1, 2] as Excel.Range).Font.Size = 16;
+            (workSheet.Cells[1, 2] as Excel.Range).Font.Bold = true;
+            (workSheet.Cells[1, 2] as Excel.Range).Font.Name = "Arial";
+            //(workSheet.Cells[1, 1] as Excel.Range).Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle = Excel.XlLineStyle.xlDash;
+
+
+            workSheet.Cells[2, 2] = "C 01 по 31";
+            (workSheet.Cells[2, 2] as Excel.Range).Font.Size = 16;
+
+            workSheet.Cells[3, 1] = "Код врача";
+            workSheet.Cells[3, 2] = "ФИО";
+            workSheet.Cells[3, 3] = "Кол-во";
+            (workSheet.Cells[3, 1] as Excel.Range).Font.Size = 14;
+            (workSheet.Cells[3, 2] as Excel.Range).Font.Size = 14;
+            (workSheet.Cells[3, 3] as Excel.Range).Font.Size = 14;
+            (workSheet.Cells[3, 1] as Excel.Range).Font.Bold = true;
+            (workSheet.Cells[3, 2] as Excel.Range).Font.Bold = true;
+            (workSheet.Cells[3, 3] as Excel.Range).Font.Bold = true;
+            (workSheet.Cells[3, 1] as Excel.Range).Font.Name = "Arial";
+            (workSheet.Cells[3, 2] as Excel.Range).Font.Name = "Arial";
+            (workSheet.Cells[3, 3] as Excel.Range).Font.Name = "Arial";
+
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    try
+                    {
+                        exApp.Cells[j + 4, i + 1] = (dataGridView1[i, j].Value).ToString();
+                    }
+                    catch {   
+                    }
+                }
+            }            
+        }
+
+        private void monthTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
